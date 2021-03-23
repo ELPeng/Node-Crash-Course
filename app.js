4b9d70715407e440e504fe03c356362e29d59d3f
EPeng <erpengic@gmail.com>
2021-03-23 12:48:59 -0500

    completed node crash course tutorial #8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 //express app
 const app = express()
@@ -9,9 +10,11 @@ app.set('view engine', 'ejs')
 //listen for requests
 app.listen(3000)
 
+//middleware & static files
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-    //res.send('<p>home page</p>')
-    // res.sendFile('./views/index.html', { root: __dirname})
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -21,8 +24,6 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    //res.send('<p>about page</p>')
-    // res.sendFile('./views/about.html', { root: __dirname})
     res.render('about', {title: 'About'})
 })
 
@@ -31,16 +32,10 @@ app.get('/blogs/create', (req, res) =>{
 })
 
 
-/*
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
-*/
+
 
 
 //404 page
 app.use((req, res) => {
-    // res.status(404).sendFile('./views/404.html', { root: __dirname})
     res.status(404).render('404', {title: '404'})
 })
